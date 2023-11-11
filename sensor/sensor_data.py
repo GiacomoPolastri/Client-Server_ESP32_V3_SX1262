f1756e57a700d131d83d39cdde03b858a97bf906
GiacomoPolastri <giacomo.polastri@gmail.com>
2023-11-11 17:52:17 +0100

    add DHTSensor class for taking sensor data

--- sensor/sensor_data.py
+++ sensor/sensor_data.py
@@ -1,7 +1,24 @@
-import dht
 from machine import Pin
-import display_out
+import dht
+import time
 
-Pin_Data = const(48)
+class DHTSensor:
+    Pin_Data = const(48)
 
-print(Pin_Data)
+    def __init__(self):
+        self.pin_data = self.Pin_Data
+        self.sensor = dht.DHT11(Pin(self.pin_data))
+
+    def measure(self):
+        self.sensor.measure()
+        temperature = self.sensor.temperature()
+        humidity = self.sensor.humidity()
+        return temperature, humidity
+
+    def get_temperature(self):
+        temperature, _ = self.measure()
+        return temperature
+
+    def get_humidity(self):
+        _, humidity = self.measure()
+        return humidity
