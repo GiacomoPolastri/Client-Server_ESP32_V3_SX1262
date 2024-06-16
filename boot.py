52197209aa2d387462e641848865fb8e58c1969a
GiacomoPolastri <giacomo.polastri@gmail.com>
2024-06-16 09:11:58 +0200

    delete sleeping time between the functions

--- boot.py
+++ boot.py
@@ -1,11 +1,8 @@
-from time import sleep_ms
 from connection.spi_connection import set_spi_connection, begin
 from connection.sender import send
 #from connection.receiver import receive
 
 sx = set_spi_connection()
-sleep_ms(1000)
 begin(sx)
-sleep_ms(1000)
 #receive(sx)
 send(sx)
